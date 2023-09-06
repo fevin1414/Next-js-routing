da6f3dd911a278f765a9b5a9fad6b1ee6d98985a
fevin1414 <fevinbiju1414@gmail.com>
2023-09-06 09:34:00 -0500

    added dynamic links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,6 +1,21 @@
-function ClientsPage(){
-  return(
-    <div>The Clients Page</div>
+import Link from "next/link";
+
+function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximillian" },
+    { id: "manu", name: "Manuel" },
+  ];
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 export default ClientsPage;
